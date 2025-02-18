4b2101c4e8cec5d787664fd779e625926ea10998
SitrakaResearchAndPOC <92709209+SitrakaResearchAndPOC@users.noreply.github.com>
2025-02-18 03:31:05 +0100

    Update mathematique.h

--- mathematique.h
+++ mathematique.h
@@ -1,28 +1,11 @@
-// mathematique.c
-#include <stdio.h>
-#include "mathematique.h"
+// mathematique.h
+#ifndef MATHEMATIQUE_H
+#define MATHEMATIQUE_H
 
-// Fonction pour additionner deux entiers
-int addition(int a, int b) {
-    return a + b;
-}
+// Prototypes des fonctions
+int addition(int a, int b);
+int soustraction(int a, int b);
+int multiplication(int a, int b);
+int division(int a, int b);
 
-// Fonction pour soustraire deux entiers
-int soustraction(int a, int b) {
-    return a - b;
-}
-
-// Fonction pour multiplier deux entiers
-int multiplication(int a, int b) {
-    return a * b;
-}
-
-// Fonction pour diviser deux entiers
-// Remarque : on suppose que b n'est pas nul.
-int division(int a, int b) {
-    if (b == 0) {
-        printf("Erreur : Division par zéro!\n");
-        return 0; // ou une autre valeur spéciale en cas d'erreur
-    }
-    return a / b;
-}
+#endif
